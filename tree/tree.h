b177257958fb8c5d8dfb7ba1c49bf0d2b600380d
Leovita <leonardo.vitale@fermi.mo.it>
2023-06-20 00:53:59 +0200

    foundamental datat structures primitives

---
+++ tree/tree.h
@@ -0,0 +1,17 @@
+struct node
+{
+   tipo_inf inf;
+   node *parent; // opzionale
+   node *firstChild;
+   node *nextSibling;
+};
+
+typedef node *tree;
+
+node *new_node(tipo_inf);
+void insert_child(tree, tree);
+void insert_sibling(node *, tree);
+tipo_inf get_info(node *);
+node *get_parent(node *);
+node *get_firstChild(node *);
+node *get_nextSibling(node *);
